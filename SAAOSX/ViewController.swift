c385ecc7db45ae574b94c46608937f9a0907ea1a
Chaitanya Kanchan <kanchanchaitanya@gmail.com>
2018-01-09 10:35:39 +0000

    Initial Commit

---
+++ SAAOSX/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SAAOSX
+//
+//  Created by Chaitanya Kanchan on 09/01/2018.
+//  Copyright © 2018 Chaitanya Kanchan. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
