7dc547d3cff6f307f5ce5246fb80cf1a57522e3a
Chaitanya Kanchan <cdkanchan@gmail.com>
2019-05-05 16:33:52 +0100

    `CloudKitPush` typealias for completion handlers on `CKRecord`

--- Common/Notes/CloudKit/CloudKitPush.swift
+++ Common/Notes/CloudKit/CloudKitPush.swift
@@ -12,7 +12,9 @@ import CDKSwiftOracc
 import os
 
 extension CloudKitNotes {
-    private func save(_ record: CKRecord, completionHandler: ((Result<CKRecord, Error>) -> Void)? = nil){
+    typealias CKCompletionHandler = ((Result<CKRecord, Error>) -> Void)?
+    
+    private func save(_ record: CKRecord, completionHandler: CKCompletionHandler = nil){
         CKContainer.default().privateCloudDatabase.save(record) { record, error in
             if let error = error {
                 completionHandler?(.failure(error))
@@ -25,19 +27,22 @@ extension CloudKitNotes {
         }
     }
     
-    func modifyRecord(_ record: CKRecord, completionHandler: ((Result<CKRecord, Error>) -> Void)? = nil) {
-        let modifyOperation = CKModifyRecordsOperation(recordsToSave: [record], recordIDsToDelete: nil)
-        modifyOperation.perRecordCompletionBlock = { record, error in
-            if let e = error {
-                completionHandler?(.failure(e))
+    func modifyRecord(_ record: CKRecord, completionHandler: CKCompletionHandler = nil) {
+        modifyRecords([record], completionHandler: completionHandler)
+    }
+    
+    func modifyRecords(_ records: [CKRecord], completionHandler: CKCompletionHandler = nil) {
+        let modifyOperation = CKModifyRecordsOperation(recordsToSave: records, recordIDsToDelete: nil)
+        modifyOperation.perRecordCompletionBlock = {record, error in
+            if let error = error {
+                completionHandler?(.failure(error))
             } else {
                 completionHandler?(.success(record))
             }
         }
-        CKContainer.default().privateCloudDatabase.add(modifyOperation)
     }
     
-    func saveNote(_ note: Note, completionHandler: ((Result<CKRecord, Error>) -> Void)? = nil) {
+    func saveNote(_ note: Note, completionHandler: CKCompletionHandler = nil) {
         guard userIsLoggedIn else {
             os_log("User is not logged into iCloud, cannot save note %s to server",
                    log: Log.CloudKit,
@@ -48,25 +53,43 @@ extension CloudKitNotes {
         save(CKRecord(note: note), completionHandler: completionHandler)
     }
     
-    func saveAnnotation(_ annotation: Annotation, completionHandler: ((Result<CKRecord, Error>) -> Void)? = nil) {
-        guard userIsLoggedIn else {return}
+    func saveAnnotation(_ annotation: Annotation, completionHandler: CKCompletionHandler = nil) {
+        guard userIsLoggedIn else {
+            os_log("User is not logged into iCloud, cannot save annotation %s to server",
+                   log: Log.CloudKit,
+                   type: .error,
+                   String(annotation.nodeReference))
+            return
+        }
         save(CKRecord(annotation: annotation), completionHandler: completionHandler)
     }
     
-    func saveTags(_ tags: UserTags, completionHandler: ((Result<CKRecord, Error>) -> Void)? = nil) {
+    func saveTags(_ tags: UserTags, completionHandler: CKCompletionHandler = nil) {
         guard userIsLoggedIn else {return}
         save(CKRecord(userTags: tags), completionHandler: completionHandler)
     }
     
-    func deleteRecord(_ record: CKRecord) {
-        let deleteOperation = CKModifyRecordsOperation(recordsToSave: nil, recordIDsToDelete: [record.recordID])
+    
+    func deleteRecord(withID id: CKRecord.ID) {
+        let deleteOperation = CKModifyRecordsOperation(recordsToSave: nil, recordIDsToDelete: [id])
         deleteOperation.modifyRecordsCompletionBlock = { _, deletedRecords, error in
             if let e = error {
-                os_log("Error deleting record with ID: %s, error: %s", log: Log.CloudKit, type: .error, String(describing: record), e.localizedDescription)
+                os_log("Error deleting record with ID: %s, error: %s",
+                       log: Log.CloudKit,
+                       type: .error,
+                       String(describing: id),
+                       e.localizedDescription)
             } else {
-                os_log("Deleted record with ID: %s", log: Log.CloudKit, type: .info, String(describing: record))
+                os_log("Deleted record with ID: %s",
+                       log: Log.CloudKit,
+                       type: .info,
+                       String(describing: id))
             }
         }
         CKContainer.default().privateCloudDatabase.add(deleteOperation)
+    }
+    
+    func deleteRecord(_ record: CKRecord) {
+        deleteRecord(withID: record.recordID)
     }
 }
