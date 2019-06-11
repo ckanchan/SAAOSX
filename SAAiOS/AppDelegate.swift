c58286176f934d8bee925c70e413164e79f5882c
Chaitanya Kanchan <cdkanchan@gmail.com>
2019-06-11 16:25:09 +0100

    - Updated to use UIScene API. Configuration for an initial window without using storyboards credit to [@ZkHaider](https://medium.com/@ZkHaider/apples-new-uiscene-api-a-programmatic-approach-52d05e382cf2)

--- SAAiOS/AppDelegate.swift
+++ SAAiOS/AppDelegate.swift
@@ -15,57 +15,24 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     var didChooseDetail = false
     
-    // Essential app services
+    // MARK:- Essential app services
     lazy var sqlDB: SQLiteCatalogue = { return SQLiteCatalogue() }()!
     lazy var glossaryDB: Glossary = { return Glossary() }()
     
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        
-//        UIView.appearance().tintColor = .purple
-        self.window = self.window ?? UIWindow()
-        let svc = UISplitViewController()
-        let detailViewController = TextEditionViewController()
-        let masterViewController = ProjectListViewController.new(detailViewController: detailViewController)
-        let detailNavigationController = UINavigationController(rootViewController: detailViewController)
-        
-        #if !targetEnvironment(UIKitForMac)
-        
-        let masterNavigationController = UINavigationController(rootViewController: masterViewController)
-        
-        masterNavigationController.isToolbarHidden = false
-        
-        svc.viewControllers = [masterNavigationController, detailNavigationController]
-        
-        #else
-        svc.viewControllers = [masterViewController, detailNavigationController]
-        
-        #endif
-        
-        svc.delegate = self
-        
-        self.window!.rootViewController = svc
-        detailViewController.navigationItem.leftBarButtonItem = svc.displayModeButtonItem
-        detailViewController.navigationItem.leftItemsSupplementBackButton = true
-        svc.primaryBackgroundStyle = .sidebar
-        
-//        self.window!.backgroundColor = .white
-        self.window!.makeKeyAndVisible()
-        
-        return true
+    // MARK:- UISceneSession Lifecycle
+    func application(_ application: UIApplication,
+                     configurationForConnecting connectingSceneSession: UISceneSession,
+                     options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
     
-}
-
-extension AppDelegate: UISplitViewControllerDelegate {
-    func splitViewController(_ splitViewController: UISplitViewController, collapseSecondary secondaryViewController: UIViewController, onto primaryViewController: UIViewController) -> Bool {
-        if
-            let navigationController = secondaryViewController as? UINavigationController,
-            navigationController.topViewController is TextEditionViewController,
-            self.didChooseDetail {
-            return false
-        } else {
-            return true
-        }
+    func application(_ application: UIApplication,
+                     didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
+    
 }
---
+++ SAAiOS/SceneDelegate.swift
@@ -0,0 +1,91 @@
+//
+//  SceneDelegate.swift
+//  SAAi
+//
+//  Created by Chaitanya Kanchan on 11/06/2019.
+//  Copyright © 2019 Chaitanya Kanchan. All rights reserved.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
+    var window: UIWindow?
+    var didChooseDetail = false
+    
+    
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
+        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
+        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        window?.windowScene = windowScene
+        
+        let svc = UISplitViewController()
+        let detailViewController = TextEditionViewController()
+        let masterViewController = ProjectListViewController.new(detailViewController: detailViewController)
+        let detailNavigationController = UINavigationController(rootViewController: detailViewController)
+        
+        #if !targetEnvironment(UIKitForMac)
+        
+        let masterNavigationController = UINavigationController(rootViewController: masterViewController)
+        
+        masterNavigationController.isToolbarHidden = false
+        
+        svc.viewControllers = [masterNavigationController, detailNavigationController]
+        
+        #else
+        svc.viewControllers = [masterViewController, detailNavigationController]
+        
+        #endif
+        
+        svc.delegate = self
+        
+        self.window!.rootViewController = svc
+        detailViewController.navigationItem.leftBarButtonItem = svc.displayModeButtonItem
+        detailViewController.navigationItem.leftItemsSupplementBackButton = true
+        svc.primaryBackgroundStyle = .sidebar
+        window?.makeKeyAndVisible()
+        
+    }
+    
+    func sceneDidDisconnect(_ scene: UIScene) {
+        // Called as the scene is being released by the system.
+        // This occurs shortly after the scene enters the background, or when its session is discarded.
+        // Release any resources associated with this scene that can be re-created the next time the scene connects.
+        // The scene may re-connect later, as its session was not neccessarily discarded (see `application:didDiscardSceneSessions` instead).
+    }
+    func sceneDidBecomeActive(_ scene: UIScene) {
+        // Called when the scene has moved from an inactive state to an active state.
+        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
+    }
+    func sceneWillResignActive(_ scene: UIScene) {
+        // Called when the scene will move from an active state to an inactive state.
+        // This may occur due to temporary interruptions (ex. an incoming phone call).
+    }
+    func sceneWillEnterForeground(_ scene: UIScene) {
+        // Called as the scene transitions from the background to the foreground.
+        // Use this method to undo the changes made on entering the background.
+    }
+    func sceneDidEnterBackground(_ scene: UIScene) {
+        // Called as the scene transitions from the foreground to the background.
+        // Use this method to save data, release shared resources, and store enough scene-specific state information
+        // to restore the scene back to its current state.
+    }
+    
+}
+
+extension SceneDelegate: UISplitViewControllerDelegate {
+    func splitViewController(_ splitViewController: UISplitViewController, collapseSecondary secondaryViewController: UIViewController, onto primaryViewController: UIViewController) -> Bool {
+        if
+            let navigationController = secondaryViewController as? UINavigationController,
+            navigationController.topViewController is TextEditionViewController,
+            self.didChooseDetail {
+            return false
+        } else {
+            return true
+        }
+    }
+}
